85d8c82390e45e873ee18acf0e71c2dd198bb067
Ashish <ashishbdatta@gmail.com>
2013-12-27 02:09:12 -0700

    init

---
+++ routes/books.js
@@ -0,0 +1,108 @@
+var mongo = require('mongodb');
+
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('bookdb', server, {safe: true});
+
+db.open(function(err, db) {
+    if(!err) {
+        console.log("Connected to 'bookdb' database");
+        db.collection('books', {safe:true}, function(err, collection) {
+            if (err) {
+                console.log("The 'books' collection doesn't exist. Creating it with sample data...");
+                populateDB();
+            }
+        });
+    }
+});
+
+exports.findById = function(req, res) {
+    var id = req.params.id;
+    console.log('Retrieving book: ' + id);
+    db.collection('books', function(err, collection) {
+        collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
+            res.send(item);
+        });
+    });
+};
+
+exports.findAll = function(req, res) {
+    db.collection('books', function(err, collection) {
+        collection.find().toArray(function(err, items) {
+            res.send(items);
+        });
+    });
+};
+
+exports.addBook = function(req, res) {
+    var book = req.body;
+    console.log('Adding book: ' + JSON.stringify(book));
+    db.collection('books', function(err, collection) {
+        collection.insert(book, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('Success: ' + JSON.stringify(result[0]));
+                res.send(result[0]);
+            }
+        });
+    });
+}
+
+exports.updateBook = function(req, res) {
+    var id = req.params.id;
+    var book = req.body;
+    delete book._id;
+    console.log('Updating book: ' + id);
+    console.log(JSON.stringify(book));
+    db.collection('books', function(err, collection) {
+        collection.update({'_id':new BSON.ObjectID(id)}, book, {safe:true}, function(err, result) {
+            if (err) {
+                console.log('Error updating book: ' + err);
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('' + result + ' document(s) updated');
+                res.send(book);
+            }
+        });
+    });
+}
+
+exports.deleteBook = function(req, res) {
+    var id = req.params.id;
+    console.log('Deleting book: ' + id);
+    db.collection('books', function(err, collection) {
+        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred - ' + err});
+            } else {
+                console.log('' + result + ' document(s) deleted');
+                res.send(req.body);
+            }
+        });
+    });
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+// Populate database with sample data -- Only used once: the first time the application is started.
+// You'd typically not find this code in a real-life app, since the database would already exist.
+var populateDB = function() {
+
+    var books = [
+    {
+        name: "I dont read",
+        year: "2009",
+        genre: "Horror",
+        country: "Murica",
+        description: "I cant even think of a book.",
+        picture: "cthulhu.jpg"
+    }];
+
+    db.collection('books', function(err, collection) {
+        collection.insert(books, {safe:true}, function(err, result) {});
+    });
+
+};
